0516b6477c9aa9fd1500dbd292231c693dd6cf64
Ahmad Syamim <ahmad.syamim@gmail.com>
2021-11-01 19:32:46 +0900

    updates : change navbar url

--- src/views/partials/header.blade.php
+++ src/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
-    <a class="navbar-item" href="https://bulma.io">
-      <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+    <a class="navbar-item" href="{{ url('/') }}">
+        <img src="{{ url('/images/logo.png') }}" alt="{{ setting('site.title') }}" title="{{ setting('site.title') }}" />
     </a>
 
     <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
